12f3b1e2cc4ab5e82f1517eb99a7946f371e16d4
GoldenTao <taoca@hotmail.com>
2021-02-21 22:19:58 +1000

    Fix for slow window drag bug [#349]

--- EarTrumpet/UI/Themes/AcrylicBrush.cs
+++ EarTrumpet/UI/Themes/AcrylicBrush.cs
@@ -1,5 +1,9 @@
-﻿using EarTrumpet.Interop.Helpers;
+﻿using EarTrumpet.Extensions;
+using EarTrumpet.Interop.Helpers;
+using System;
 using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Threading;
 
 namespace EarTrumpet.UI.Themes
 {
@@ -9,11 +13,40 @@ namespace EarTrumpet.UI.Themes
         public static void SetBackground(DependencyObject obj, string value) => obj.SetValue(BackgroundProperty, value);
         public static readonly DependencyProperty BackgroundProperty =
         DependencyProperty.RegisterAttached("Background", typeof(string), typeof(AcrylicBrush), new PropertyMetadata("", BackgroundChanged));
+
+        public static bool GetIsSuppressed(DependencyObject obj) => (bool)obj.GetValue(IsSuppressedProperty);
+        public static void SetIsSuppressed(DependencyObject obj, bool value) => obj.SetValue(IsSuppressedProperty, value);
+        public static readonly DependencyProperty IsSuppressedProperty =
+        DependencyProperty.RegisterAttached("IsSuppressed", typeof(bool), typeof(AcrylicBrush), new PropertyMetadata(false));
+        
         private static void BackgroundChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
         {
             var window = (Window)dependencyObject;
+            var locationChangedTimer = new DispatcherTimer();
+
             window.Closed += (_, __) => window = null;
             window.SourceInitialized += (_, __) => ApplyAcrylicToWindow(window, (string)e.NewValue);
+            window.LocationChanged += (_, __) =>
+            {
+                if ((HwndSource)PresentationSource.FromVisual(window) != null)
+                {
+                    if (!locationChangedTimer.IsEnabled)
+                    {
+                        SetIsSuppressed(window, true);
+                        Dispatcher.CurrentDispatcher.InvokeAsync(() => UpdateWindowAcrylic(window));
+                    }
+                    locationChangedTimer.Stop();
+                    locationChangedTimer.Start();
+                }
+            };
+
+            locationChangedTimer.Interval = TimeSpan.FromMilliseconds(200);
+            locationChangedTimer.Tick += (_, __) =>
+            {
+                locationChangedTimer.Stop();
+                SetIsSuppressed(window, false);
+                UpdateWindowAcrylic(window);
+            };
 
             Manager.Current.ThemeChanged += () =>
             {
@@ -29,6 +62,19 @@ namespace EarTrumpet.UI.Themes
             AccentPolicyLibrary.EnableAcrylic(window,
                 Manager.Current.ResolveRef(window, refValue),
                 Interop.User32.AccentFlags.DrawAllBorders);
+        }
+
+        private static void UpdateWindowAcrylic(Window window)
+        {
+            var suppressed = GetIsSuppressed(window);
+            if (suppressed) 
+            {
+                AccentPolicyLibrary.DisableAcrylic(window);
+            } 
+            else 
+            {
+                ApplyAcrylicToWindow(window, GetBackground(window)); 
+            }
         }
     }
 }
